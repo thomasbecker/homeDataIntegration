6267e1a80bce704b0530bc5e2de74377ad70919c
Thomas Becker <thomas.becker00@gmail.com>
2018-01-12 20:14:03 +0100

    model refactoring

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.12.4"
 
 resolvers += Resolver.mavenLocal
 
+libraryDependencies += "com.typesafe.play" %% "play-json" % "2.6.7"
 libraryDependencies += "de.softwareschmied" %% "solarweb-interface" % "0.0.1-SNAPSHOT"
 libraryDependencies += "de.softwareschmied" %% "myhomecontrol-interface" % "0.0.1-SNAPSHOT"
--- src/main/scala/de/softwareschmied/homedataintegration/HomeCollector.scala
+++ src/main/scala/de/softwareschmied/homedataintegration/HomeCollector.scala
@@ -2,14 +2,23 @@ package de.softwareschmied.homedataintegration
 
 import java.time.Instant
 
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import com.typesafe.scalalogging.Logger
 import de.softwareschmied.myhomecontrolinterface.{MyHomeControlCollector, MyHomeControlData}
 import de.softwareschmied.solarwebinterface.{PowerFlowSite, SolarWebConnector}
+import play.api.libs.json.{Format, Json}
+import spray.json.{DefaultJsonProtocol, NullOptions}
 
 /**
  * Created by Thomas Becker (thomas.becker00@gmail.com) on 28.11.17.
  */
-case class HomeData(powerFlowSite: PowerFlowSite, myHomeControlData: MyHomeControlData, timestamp: Long = Instant.now.getEpochSecond)
+case class HomeData(powerGrid: Double, powerLoad: Double, powerPv: Option[Double], selfConsumption: Option[Double],
+                    autonomy: Option[Double], heatpumpPowerConsumption: Double, livingRoomTemp: Double, sleepingRoomCo2: Double,
+                    timestamp: Long = Instant.now.toEpochMilli)
+
+trait HomeDataJsonSupport extends SprayJsonSupport with DefaultJsonProtocol with NullOptions {
+  implicit val homeDataFormat = jsonFormat9(HomeData)
+}
 
 class HomeCollector {
   val logger = Logger[HomeCollector]
@@ -19,7 +28,8 @@ class HomeCollector {
   def collectData: HomeData = {
     val myHomeControlData = myHomeControlCollector.collectMyHomeControlData()
     val powerFlowSite = solarWebConnector.getPowerFlowRealtimeData()
-    val homeData = new HomeData(powerFlowSite, myHomeControlData)
+    val homeData = new HomeData(powerFlowSite.powerGrid, powerFlowSite.powerLoad, powerFlowSite.powerPv, powerFlowSite.selfConsumption, powerFlowSite
+      .autonomy, myHomeControlData.heatpumpPowerConsumption, myHomeControlData.livingRoomTemp, myHomeControlData.sleepingRoomCo2)
     logger.info(s"HomeData: $homeData")
     homeData
   }
